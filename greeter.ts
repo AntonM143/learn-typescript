f25fd046103aa85544c8c4054256eae4e2d8facf
Anton Månsson <anton.mansson@mediainstitutet.se>
2021-05-17 17:45:10 +0200

    tutorial

---
+++ greeter.ts
@@ -0,0 +1,23 @@
+class Student {
+  fullName: string;
+  constructor(
+    public firstName: string,
+    public middleInitial: string,
+    public lastName: string
+  ) {
+    this.fullName = firstName + " " + middleInitial + " " + lastName;
+  }
+}
+
+interface Person {
+  firstName: string;
+  lastName: string;
+  middleInitial: string;
+}
+
+function greeter(person: Person) {
+  return "Hello, " + person.firstName + " " + person.middleInitial + " " + person.lastName;
+}
+
+let user = new Student("janne", "granne", "doe");
+console.log(greeter(user));
